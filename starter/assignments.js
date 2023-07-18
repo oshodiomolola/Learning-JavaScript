5de5ac6c1eab209588cc31e7e6e9ba575e3a654d
oshodiomolola <iamzoey323@gmail.com>
2023-07-18 22:27:21 +0100

    my-first-commit

---
+++ starter/assignments.js
@@ -0,0 +1,9 @@
+/*
+let country = "Nigeria";
+let continent = "Africa";
+let population = " 221,560,158";
+
+console.log(country);
+console.log(continent);
+console.log(population)
+*/
---
+++ starter/script.js
@@ -0,0 +1,106 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('Lola');
+console.log('23');
+
+let firstName = "Lola";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName); 
+
+// variable name conventions
+let lola_crown = "LM";
+let $function = 27;
+// name is a key word but still legal to use
+// it can cause a lot of confusion Sha, so use firstName instead
+let name = 'lola';
+// PI value is a known constant, so it should be in uppercase
+let PI = 3.1415;
+
+let myFirstJob = "fashion manager";
+let myCurrentJob = 'programmer';
+
+console.log(myFirstJob);
+*/
+
+/*
+// data types
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+/*console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'lola');
+console.log(typeof localStorage);
+console.log(typeof lola);
+
+console.log(typeof javaScriptIsFun);
+
+
+javaScriptIsFun = 'YES!'
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(year);
+console.log(typeof year);
+
+// the datatype is considered a bug or an error cause it shoukd have printed out null not object
+console.log(typeof null)
+
+const birthYear = 1997;
+console.log(birthYear);
+// birthYear = 2000;  this is wrong cause the value of const can't be changed
+  
+// const job; this is wrong cause const has to ALWAYS have a value assigned to it
+
+var job = 'programmer';
+console.log(job);
+job = "manager";
+console.log(job)
+
+bestFriend = 'Belle';
+console.log(bestFriend);
+
+// math operators
+const now = 2023
+const ageLola = now - 1997;
+const ageBelle = now - 1992
+console.log(ageLola, ageBelle);
+
+console.log(ageLola * 2, ageLola / 2, ageBelle /3, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Crown';
+const lastName = 'Oshodi';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+ let x = 10 + 5;   //15
+ x += 10;   // x = x + 10 = 25
+ x *= 4;  // x = x * 4 = 100
+ x ++;
+ x --;
+console.log(x);
+
+// comparism operators
+console.log(ageLola > ageBelle); 
+// > , <, >=, <=
+console.log(ageBelle >= 18);
+
+const isFullAge = ageLola >= 18;
+
+console.log(now - 1997 > now - 2020)
+*/
+
+const now = 2023;
+const ageLola = now - 1997;
+const ageBelle = now - 1992;
+console.log(ageLola, ageBelle);
+
+console.log(now - 1997 > now - 2020);
